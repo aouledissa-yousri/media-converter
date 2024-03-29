0ae93922d4f0f76534a98476a723e14a84ebf187
aouledissa <aouledissayousri@gmail.com>
2024-03-29 23:20:47 +0100

    added video conversion

---
+++ video-format-conversion/flv-conversion/flv_avi_conversion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <filesystem>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    filesystem::path filePath(argv[1]);
+
+    string command = "ffmpeg -i " + filePath.generic_string() + " " + filePath.filename().stem().string() + ".avi";
+    system(command.c_str());
+
+    return 0;
+}
