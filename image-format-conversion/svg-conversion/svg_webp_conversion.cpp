166675f88f6bb75347b08e9a54357975e248892b
aouledissa <aouledissayousri@gmail.com>
2024-03-29 06:27:43 +0100

    first commit

---
+++ image-format-conversion/svg-conversion/svg_webp_conversion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <filesystem>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    filesystem::path filePath(argv[1]);
+
+    string command = "convert " + filePath.generic_string() + " " + filePath.filename().stem().string() + ".webp";
+    system(command.c_str());
+
+    return 0;
+}
