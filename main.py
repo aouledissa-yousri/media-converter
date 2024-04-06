2a70992da730c1277269f93d01389bed11f60b45
aouledissa <aouledissayousri@gmail.com>
2024-04-06 06:32:30 +0100

    final commit

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from settings import apps
+from fastapi.middleware.cors import CORSMiddleware
+from settings import origins
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
 
 for route, subApp in apps.items(): 
     app.include_router(subApp, prefix=f"/{route}")
--- settings.py
+++ settings.py
@@ -4,6 +4,13 @@ from endpoints.video import *
 from endpoints.video_audio import *
 
 
+# Define allowed origins
+origins = [
+    "http://localhost:3000",
+    # Add any other allowed origins here
+]
+
+
 apps = {
     "m4a": m4a, 
     "mp3": mp3, 
